7640816776dfcebbbe75033905754e20cd746d68
leacloud <13883721874@163.com>
2022-12-02 22:28:28 +0800

    feat: 重置表单

--- src/pages/home/components/ChildComponent.vue
+++ src/pages/home/components/ChildComponent.vue
@@ -1,11 +1,28 @@
 <template>
-  <h5>我是子组件中展示的store信息：模块名称：{{ modulename }}</h5>
+  <section>
+    <h5>我是子组件中展示的store信息：模块名称：{{ modulename }}</h5>
+
+    <input
+      type="text"
+      v-model="modulename"
+      placeholder="输入你想要修改的名字"
+    />
+    <buttonstore @click="onReset">重置</buttonstore>
+  </section>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import { useUserStore } from "@/store/user.js";
 
 const store = useUserStore();
 const { modulename } = storeToRefs(store);
+
+/**
+ * 重置
+ */
+const onReset = () => {
+  store.$reset();
+};
 </script>
