d9620f3e2d1f0373e665bbbf0c0e3f3ca6328de5
leacloud <13883721874@163.com>
2022-12-02 21:08:35 +0800

    refactor: 初始化项目

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      redirect: "/home",
+    },
+    {
+      path: "/home",
+      name: "Home",
+      component: () => import("@/pages/home/index.vue"),
+    },
+  ],
+});
+
+export default router;
