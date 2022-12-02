579b340383f5e4743ab9e7972f083b6a3a5de2c7
leacloud <13883721874@163.com>
2022-12-02 22:00:38 +0800

    feat: 子组件通信

---
+++ src/pages/home/components/ChildComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h5>我是子组件中展示的store信息：模块名称：{{ store.modulename }}</h5>
+</template>
+
+<script setup>
+import { useUserStore } from "@/store/user.js";
+
+const store = useUserStore();
+</script>
--- src/pages/home/homePage.vue
+++ src/pages/home/homePage.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <h1>模块名称：{{ store.modulename }}</h1>
+    <ChildComponent />
   </div>
 </template>
 
 <script setup>
 import { useUserStore } from "@/store/user";
+import ChildComponent from "./components/ChildComponent.vue";
 
 const store = useUserStore();
 </script>
