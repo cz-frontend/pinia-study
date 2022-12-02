5f572620540f3ccd2ef5b1b4373d7ba2574d8622
leacloud <13883721874@163.com>
2022-12-02 22:17:31 +0800

    feat: storeToRefs形式修改store属性

--- src/pages/home/components/ChildComponent.vue
+++ src/pages/home/components/ChildComponent.vue
@@ -1,9 +1,11 @@
 <template>
-  <h5>我是子组件中展示的store信息：模块名称：{{ store.modulename }}</h5>
+  <h5>我是子组件中展示的store信息：模块名称：{{ modulename }}</h5>
 </template>
 
 <script setup>
+import { storeToRefs } from "pinia";
 import { useUserStore } from "@/store/user.js";
 
 const store = useUserStore();
+const { modulename } = storeToRefs(store);
 </script>
--- src/pages/home/homePage.vue
+++ src/pages/home/homePage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>模块名称：{{ store.modulename }}</h1>
+    <h1>模块名称：{{ modulename }}</h1>
     <ChildComponent />
 
     <button @click="onRename">更新模块名称</button>
@@ -8,10 +8,13 @@
 </template>
 
 <script setup>
+import { storeToRefs } from "pinia";
 import { useUserStore } from "@/store/user";
 import ChildComponent from "./components/ChildComponent.vue";
 
 const store = useUserStore();
+const { modulename } = storeToRefs(store);
+
 const onRename = () => {
   store.modulename = "用户管理";
 };
