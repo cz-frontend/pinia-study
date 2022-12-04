695b5d8f6efeceaeb5f5d0dbed167c6031512624
leacloud <13883721874@163.com>
2022-12-04 10:36:20 +0800

    config: 集成Element-Plus

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
 const app = createApp(App);
 const store = createPinia();
 
+app.use(ElementPlus);
 app.use(store);
 app.use(router);
 
